256127febade8388bb782897174588770e21141d
rohan3155 <bhatiarohan050@gmail.com>
2026-03-10 15:35:34 +0530

    feat: Externalize API base URL to environment variables and add .env to gitignore.

--- hrms-client/src/api/attendances.js
+++ hrms-client/src/api/attendances.js
@@ -1,6 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 
-const API_BASE = 'http://localhost:8000'
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 
 export const getAttendances = async ({ skip = 0, limit = 10, search = '', sort_by = 'id', order = 'asc' }) => {
