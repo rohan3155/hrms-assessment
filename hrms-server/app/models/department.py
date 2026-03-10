513440930c8a66ab615d9fcb6f371761487233ff
rohan3155 <bhatiarohan050@gmail.com>
2026-03-10 09:56:49 +0530

    feat: added department model

---
+++ hrms-server/app/models/department.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Department(Base):
+        __tablename__ = "departments"
+        id = Column(Integer, primary_key=True, index=True)
+        name = Column(String)
+        employees = relationship("Employee", back_populates="department")
