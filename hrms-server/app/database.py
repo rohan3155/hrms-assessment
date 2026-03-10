0c2bf5ef722e8b5f846d040f8ac2cafe1de1bedd
rohan3155 <bhatiarohan050@gmail.com>
2026-03-10 09:49:37 +0530

    feat: added get_db method to get database session

--- hrms-server/app/database.py
+++ hrms-server/app/database.py
@@ -11,3 +11,10 @@ engine = create_engine(DATABASE_URL or "sqlite:///./hrms.db")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+        db = SessionLocal()
+        try:
+                yield db
+        finally:
+                db.close()
